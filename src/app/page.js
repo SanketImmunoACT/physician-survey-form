055bac11190850243dc6762d5352a9a178ee36bb
SanketImmunoACT <sanket.rathod@immunoact.com>
2025-08-04 11:16:28 +0530

    Minor Changes.

--- src/app/page.js
+++ src/app/page.js
@@ -25,14 +25,20 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  
-  useEffect(() => {
-    // Redirect to auth page when app starts
-    const takeMeToAuth = () => {
-    router.push("/auth")}
 
-    setTimeout(takeMeToAuth,2000) 
-  }, [router]);
+  // useEffect(() => {
+  //   // Redirect to auth page when app starts
+  //   // const takeMeToAuth = () => {
+  //   router.push("/auth")}
+
+  //   // setTimeout(takeMeToAuth,2000) 
+  // }, [router]);
+
+
+
+  useEffect(() => {
+    router.push("/auth")
+  }, [router])
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
